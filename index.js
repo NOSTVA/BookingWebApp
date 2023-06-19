d7c7d931102d1848210764e5de412e0b25f9806b
NOSTVA <nostva9000@gmail.com>
2023-06-19 16:16:46 +0300

    asd

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const app = require("./src/config/app");
 const connectDB = require("./src/config/database");
-
 require("dotenv").config();
 
 connectDB(process.env.MONGO_URI)
   .then(() => {
-    console.log(`MonogoDB running on ${process.env.MONGO_URI}`);
-    app.listen(process.env.PORT, () => {
-      console.log(
-        `Server live at http://${process.env.HOST}:${process.env.PORT}`
-      );
-    });
+    console.log(`MongoDB running on ${process.env.MONGO_URI}`);
   })
   .catch(console.log);
 
-export default app;
+module.exports = (req, res) => {
+  app(req, res);
+};
