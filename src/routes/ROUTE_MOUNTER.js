e70573720776003709aab0f93cc59f8bc6add27a
NOSTVA <nostva9000@gmail.com>
2023-07-07 17:08:47 +0300

    router mount fixes

--- src/routes/ROUTE_MOUNTER.js
+++ src/routes/ROUTE_MOUNTER.js
@@ -9,11 +9,24 @@ const userRouter = require("../routes/userRouter");
 const appointmentsRouter = require("../routes/appointmentsRouter");
 const applicantsRouter = require("../routes/applicantsRouter");
 
-app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/v2", authRouter);
 app.use("/api/v2/users", isAuthenticated, requireAdmin, userRouter);
 app.use("/api/v2/appointments", isAuthenticated, appointmentsRouter);
 app.use("/api/v2/applicants", isAuthenticated, applicantsRouter);
-app.get("*", (req, res) => {
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/create", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/team/auth/register", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
